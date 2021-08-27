1651a4b684b203bbd8155460863bb98567353b7c
Michal Zabinski <zabinski@student.agh.edu.pl>
2021-08-27 14:06:01 +0200

    Werscja bez korzystania z Jsona

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import com.google.gson.Gson;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Question question1 = new Question("Which of the cointries have the biggest population?", "A - Poland", "B - Brasil", "C - India", "D - China", "D");
+        Question question2 = new Question("Jak powiedziec który po angielsku?", "A - Który", "B - Bury", "C - Which", "D - Witch", "C");
+        ArrayList<Question> questions = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        int score = 0;
+
+        questions.add(question1);
+        questions.add(question2);
+
+        for (Question task : questions) {
+            System.out.println(task.getQuestion());
+
+            System.out.println(task.getAnswerA());
+            System.out.println(task.getAnswerB());
+            System.out.println(task.getAnswerC());
+            System.out.println(task.getAnswerD());
+
+            System.out.println();
+            System.out.println("Choose correct answer");
+
+            String input = scanner.nextLine();
+            if(input == task.getCorrectAnswer()){
+                System.out.println("Good choice, you get a point");
+                score++;
+            }else{
+                System.out.println("Fortunately:(");
+            }
+
+        }
+    }
+}
---
+++ src/main/java/Question.java
@@ -0,0 +1,44 @@
+public class Question {
+    private String question;
+
+    private String answerA;
+    private String answerB;
+    private String answerC;
+    private String answerD;
+
+    private String  correctAnswer;
+
+    public Question(String question, String answerA, String answerB, String answerC, String answerD, String correctAnswer) {
+        this.question = question;
+        this.answerA = answerA;
+        this.answerB = answerB;
+        this.answerC = answerC;
+        this.answerD = answerD;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestion(){
+        return question;
+    }
+
+    public String getAnswerA(){
+        return answerA;
+    }
+
+    public String getAnswerB() {
+        return answerB;
+    }
+
+    public String getAnswerC() {
+        return answerC;
+    }
+
+    public String getAnswerD() {
+        return answerD;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+}
